067dfd79ac1c43db174db7f613234cd2e9f22756
Nehuen Lian <nehuenn.lian@gmail.com>
2025-12-27 14:55:33 -0300

    remove dead code and add comments

--- service/payload_builder/builder.py
+++ service/payload_builder/builder.py
@@ -1,8 +1,9 @@
 from service.utils.logger import logger
 
 
+# Used in controllers.request_invoice_controller.py
 def add_auth_to_payload(sale_data: dict, token: str, sign: str) -> dict:
-    
+
     sale_data['Auth']['Token'] = token
     sale_data['Auth']['Sign'] = sign
     logger.debug("Auth added to payload.")
@@ -18,28 +19,3 @@ def build_auth(token: str, sign: str, cuit: int) -> dict:
     }
 
     return auth
-
-def extract_cbtenro(last_authorized_info: dict) -> int:
-    """
-    last_authorized_info structure:
-
-    last_authorized_info = {
-        'PtoVta': 1,
-        'CbteTipo': 6,
-        'CbteNro': 69,
-        'Errors': None,
-        'Events': None
-    }
-
-    """
-    cbte_nro =  last_authorized_info['CbteNro']
-    return cbte_nro
-
-def update_sale_data(parsed_data: dict, cbte_nro: int) -> dict:
-
-    parsed_data['FeCAEReq']['FeDetReq']['FECAEDetRequest']['CbteDesde'] = cbte_nro + 1
-    parsed_data['FeCAEReq']['FeDetReq']['FECAEDetRequest']['CbteHasta'] = cbte_nro + 1
-    
-    updated_invoice = parsed_data
-
-    return updated_invoice
