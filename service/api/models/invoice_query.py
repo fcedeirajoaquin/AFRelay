9bd79d34f2f126c8a0bfb32e0c8276000991d440
Nehuen Lian <nehuenn.lian@gmail.com>
2026-02-08 01:57:14 -0300

    add model for FECompTotXRequest endpoint

--- service/api/models/invoice_query.py
+++ service/api/models/invoice_query.py
@@ -4,15 +4,18 @@ from pydantic import BaseModel
 class InvoiceBaseQuery(BaseModel):
     Cuit: int
 
-class FECAEAConsultar(InvoiceBaseQuery):
-    Periodo: int
-    Orden: int
+class FECompTotXRequest(InvoiceBaseQuery):
+    pass
+
+class FECompUltimoAutorizado(InvoiceBaseQuery):
+    PtoVta: int
+    CbteTipo: int
 
 class FECompConsultar(InvoiceBaseQuery):
     PtoVta: int
     CbteTipo: int
     CbteNro: int
 
-class FECompUltimoAutorizado(InvoiceBaseQuery):
-    PtoVta: int
-    CbteTipo: int
+class FECAEAConsultar(InvoiceBaseQuery):
+    Periodo: int
+    Orden: int
